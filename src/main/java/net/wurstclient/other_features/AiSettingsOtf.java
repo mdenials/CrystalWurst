8a9c9aea2d898f86e295f67c315d1ece1e934652
mdenials <46415607+mdenials@users.noreply.github.com>
2024-08-17 19:40:57 +0700

    Update AiSettingsOtf.java

--- src/main/java/net/wurstclient/other_features/AiSettingsOtf.java
+++ src/main/java/net/wurstclient/other_features/AiSettingsOtf.java
@@ -15,14 +15,14 @@ public final SliderSetting range = new SliderSetting("Search Range","Max search 
 public final SliderSetting thinkSpeed = new SliderSetting("ThinkSpeed","ThinkSpeed value.", 100, 0, 2147483647, 1, ValueDisplay.INTEGER);
 public final SliderSetting thinkTime = new SliderSetting("ThinkTime","ThinkTime value.", 100, 0, 2147483647, 1, ValueDisplay.INTEGER);
 public final SliderSetting renderedThings = new SliderSetting("RenderedThings","RenderedThings max value.", 100, 0, 5000, 1, ValueDisplay.INTEGER);
-public final SliderSetting standartCost = new SliderSetting("Standart Cost","ThinkTime value.", 0.5, 0, 2000, 1, ValueDisplay.DECIMAL);
-public final SliderSetting modifierCost = new SliderSetting("Cost Modifier","Cost modifier value.", 0.5, 0, 2000, 1, ValueDisplay.DECIMAL);
-public final SliderSetting walkingCost = new SliderSetting("Cost Walking","Cost walking value.", 0.5, 0, 2000, 1, ValueDisplay.DECIMAL);
-public final SliderSetting diagonalCost = new SliderSetting("Cost Diagonal Waking","Cost diagonal walking value.", 0.5, 0, 2000, 1, ValueDisplay.DECIMAL);
-public final SliderSetting liquidsCost = new SliderSetting("Cost Liquids","Cost liquids value.", 0.5, 0, 2000, 1, ValueDisplay.DECIMAL);
-public final SliderSetting slownessCost = new SliderSetting("Cost Slowness","Cost slowness value.", 0.5, 0, 2000, 1, ValueDisplay.DECIMAL);
-public final SliderSetting miningCost = new SliderSetting("Cost Mining","Cost mining value.", 0.5, 0, 2000, 1, ValueDisplay.DECIMAL);
-public final SliderSetting jumpingCost = new SliderSetting("Cost Jumping","Cost jumping value.", 0.5, 0, 2000, 1, ValueDisplay.DECIMAL);
+public final SliderSetting standartCost = new SliderSetting("Standart Cost","ThinkTime value.", 0.5, 0, 2000, 0.000001, ValueDisplay.DECIMAL);
+public final SliderSetting modifierCost = new SliderSetting("Cost Modifier","Cost modifier value.", 0.5, 0, 2000, 0.000001, ValueDisplay.DECIMAL);
+public final SliderSetting walkingCost = new SliderSetting("Cost Walking","Cost walking value.", 0.5, 0, 2000, 0.000001, ValueDisplay.DECIMAL);
+public final SliderSetting diagonalCost = new SliderSetting("Cost Diagonal Waking","Cost diagonal walking value.", 0.5, 0, 2000, 0.000001, ValueDisplay.DECIMAL);
+public final SliderSetting liquidsCost = new SliderSetting("Cost Liquids","Cost liquids value.", 0.5, 0, 2000, 0.000001, ValueDisplay.DECIMAL);
+public final SliderSetting slownessCost = new SliderSetting("Cost Slowness","Cost slowness value.", 0.5, 0, 2000, 0.000001, ValueDisplay.DECIMAL);
+public final SliderSetting miningCost = new SliderSetting("Cost Mining","Cost mining value.", 0.5, 0, 2000, 0.000001, ValueDisplay.DECIMAL);
+public final SliderSetting jumpingCost = new SliderSetting("Cost Jumping","Cost jumping value.", 0.5, 0, 2000, 0.000001, ValueDisplay.DECIMAL);
 
 public AiSettingsOtf()
 {
