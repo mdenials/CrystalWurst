49142d4a65ba78ee6cd750ded0d81e74035f1083
mdenials <46415607+mdenials@users.noreply.github.com>
2024-08-20 18:27:06 +0700

    Update TrueSightHack.java

--- src/main/java/net/wurstclient/hacks/TrueSightHack.java
+++ src/main/java/net/wurstclient/hacks/TrueSightHack.java
@@ -17,28 +17,7 @@ import net.wurstclient.settings.filters.*;
 @SearchTags({"true sight"})
 public final class TrueSightHack extends Hack
 {
-	private final EntityFilterList entityFilters =
-		new EntityFilterList(FilterHostileSetting.genericVision(false),
-			FilterNeutralSetting
-				.genericVision(AttackDetectingEntityFilter.Mode.OFF),
-			FilterPassiveSetting.genericVision(false),
-			FilterPassiveWaterSetting.genericVision(false),
-			FilterBatsSetting.genericVision(false),
-			FilterSlimesSetting.genericVision(false),
-			FilterPetsSetting.genericVision(false),
-			FilterVillagersSetting.genericVision(false),
-			FilterZombieVillagersSetting.genericVision(false),
-			FilterGolemsSetting.genericVision(false),
-			FilterPiglinsSetting
-				.genericVision(AttackDetectingEntityFilter.Mode.OFF),
-			FilterZombiePiglinsSetting
-				.genericVision(AttackDetectingEntityFilter.Mode.OFF),
-			FilterEndermenSetting
-				.genericVision(AttackDetectingEntityFilter.Mode.OFF),
-			FilterShulkersSetting.genericVision(false),
-			FilterAllaysSetting.genericVision(false),
-			FilterNamedSetting.genericVision(false),
-			FilterArmorStandsSetting.genericVision(false));
+	private final EntityFilterList entityFilters = EntityFilterList.genericVision();
 	
 	public TrueSightHack()
 	{
