21c8b0e4f483e3ce1daf2ac17969e6a615e6acaa
mdenials <46415607+mdenials@users.noreply.github.com>
2024-08-22 01:50:32 +0700

    Update GenericContainerScreenMixin.java

--- src/main/java/net/wurstclient/mixin/GenericContainerScreenMixin.java
+++ src/main/java/net/wurstclient/mixin/GenericContainerScreenMixin.java
@@ -60,6 +60,10 @@ public abstract class GenericContainerScreenMixin
 			addDrawableChild(ButtonWidget
 				.builder(Text.literal("Store"), b -> store())
 				.dimensions(x + backgroundWidth - 56, y + 4, 50, 12).build());
+
+			addDrawableChild(ButtonWidget
+				.builder(Text.literal("Sort"), b -> sort())
+				.dimensions(x + backgroundWidth - 160, y + 4, 50, 12).build());
 		}
 		
 		if(autoSteal.isEnabled())
@@ -74,6 +78,11 @@ public abstract class GenericContainerScreenMixin
 	private void store()
 	{
 		runInThread(() -> shiftClickSlots(rows * 9, rows * 9 + 44, 2));
+	}
+
+	private void sort()
+	{
+		runInThread(() -> sortInThread(0, rows * 9, 3));
 	}
 	
 	private void runInThread(Runnable r)
@@ -118,5 +127,159 @@ public abstract class GenericContainerScreenMixin
 		{
 			throw new RuntimeException(e);
 		}
+	}
+
+	private void sortInThread(int from, int to, int mode)
+	{
+		this.mode = mode;
+
+		final List<Integer> orderedSlots = getOrderedSlots(from, to);
+		final Set<Integer> remainingSlots = new HashSet<>(orderedSlots);
+		final Map<Integer, Integer> oldPosToNewPosMapping = getOldPosToNewPosMapping(orderedSlots);
+
+		if(remainingSlots.stream().findFirst().isPresent())
+			runInThread(() -> replaceSlot(remainingSlots, oldPosToNewPosMapping, -1));
+	}
+
+	/**
+	 * Recursive method to sort a single Slot
+	 * The break condition is `remainingSlots` being empty.
+	 *
+	 * @param remainingSlots A set of remaining slot positions that are not sorted yet
+	 * @param oldPosToNewPosMapping A map binding old slot positions to their new sorted one
+	 * @param cursorOldPosition The slot position from which the current Cursor Stack originated
+	 */
+	private void replaceSlot(final Set<Integer> remainingSlots, final Map<Integer, Integer> oldPosToNewPosMapping, final int cursorOldPosition) {
+
+		final int sourcePosition;
+		final int targetPosition;
+		ItemStack cursorStack = handler.getCursorStack();
+
+		if(cursorStack.isEmpty())
+		{
+            final Optional<Integer> remainingSlotOpt = remainingSlots.stream().findFirst();
+            final int nextSourcePosition = remainingSlotOpt.get();
+
+			sourcePosition = nextSourcePosition;
+			targetPosition = oldPosToNewPosMapping.get(nextSourcePosition);
+
+			if(remainingSlotOpt.isEmpty())
+				return;
+		}
+		else
+		{
+			sourcePosition = cursorOldPosition;
+			targetPosition = oldPosToNewPosMapping.get(cursorOldPosition);
+		}
+
+		if(sourcePosition != targetPosition)
+		{
+			if(cursorStack.isEmpty()) {
+				if(pickUpSlotWithDelay(sourcePosition))
+					return;
+			}
+			if(pickUpSlotWithDelay(targetPosition))
+				return;
+		}
+
+		remainingSlots.remove(sourcePosition);
+		replaceSlot(remainingSlots, oldPosToNewPosMapping, targetPosition);
+	}
+
+	/**
+	 * Consumes a list of ordered slot positions and produces a mapping of their current positions to their new one
+	 *
+	 * @param orderedSlots value example: [0, 1, 5, 6, 11, 12, 13, 15, 16, 17, 20, 22, 23]
+	 * @return value example: {0=0, 1=1, 5=2, 6=3, 11=4, 12=5, 13=6, 15=7, 16=8, 17=9, 20=10, 22=11, 23=12}
+	 */
+	private Map<Integer, Integer> getOldPosToNewPosMapping(final List<Integer> orderedSlots) {
+		final Map<Integer, Integer> oldPosToNewPosMapping = new HashMap<>();
+		int counter = 0;
+
+		for(int slotNum : orderedSlots)
+		{
+			oldPosToNewPosMapping.put(slotNum, counter);
+			++counter;
+		}
+
+		return oldPosToNewPosMapping;
+	}
+
+	/**
+	 * Produces a list of ordered slot positions containing items, sorted by item names.
+	 *
+	 * @param from Lower slot position bound (included)
+	 * @param to Upper slot position bound (excluded)
+	 * @return list of ordered slots
+	 */
+	private List<Integer> getOrderedSlots(final int from, final int to)
+	{
+		Map<String, List<Integer>> slotMap = getItemNameToSlotPositionsMap(from, to);
+		List<Integer> orderedSlots = new LinkedList<>();
+
+		for(String itemName : slotMap.keySet())
+		{
+			final List<Integer> sameItemNameSlotPositions = slotMap.get(itemName);
+
+			for(int slot : sameItemNameSlotPositions)
+				orderedSlots.add(slot);
+		}
+
+		return orderedSlots;
+	}
+
+	/**
+	 *
+	 * @param from Lower slot position bound (included)
+	 * @param to Upper slot position bound (excluded)
+	 * @return map of slot positions, sorted by item names
+	 */
+	private Map<String, List<Integer>> getItemNameToSlotPositionsMap(final int from, final int to)
+	{
+		Map<String, List<Integer>> itemNameToSlotPositionsMap = new HashMap<>();
+		for(int i = from; i < to; ++i)
+		{
+			final Slot slot = handler.slots.get(i);
+
+			if(slot.getStack().isEmpty())
+				continue;
+
+			final String itemName = slot.getStack().getItem().getName().getString();
+
+			if(itemNameToSlotPositionsMap.containsKey(itemName))
+				itemNameToSlotPositionsMap.get(itemName).add(i);
+			else
+				itemNameToSlotPositionsMap.put(itemName, new LinkedList<>(List.of(i)));
+		}
+
+		return itemNameToSlotPositionsMap;
+	}
+
+	/**
+	 * Wrapper for pickup slot action with added delay and cancellation detection
+	 *
+	 * @param sourcePosition
+	 * @return
+	 */
+	private boolean pickUpSlotWithDelay(int sourcePosition) {
+		Slot oldSlot = handler.slots.get(sourcePosition);
+
+		waitForDelay();
+
+		if(mustCancelOperation(mode))
+			return true;
+
+		onMouseClick(oldSlot, oldSlot.id, 0, SlotActionType.PICKUP);
+		return false;
+	}
+
+	/**
+	 * The user chose a new action among the available ones (Sort, Steal, Sort), or closed the Container GUI.
+	 *
+	 * @param mode
+	 * @return
+	 */
+	private boolean mustCancelOperation(int mode) {
+		return this.mode != mode || client.currentScreen == null;
 	}
 }
