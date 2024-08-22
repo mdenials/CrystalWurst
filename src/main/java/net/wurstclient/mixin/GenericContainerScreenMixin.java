a47758357d64cee3105497974439fdd8d9810d71
mdenials <46415607+mdenials@users.noreply.github.com>
2024-08-22 14:40:01 +0700

    Update GenericContainerScreenMixin.java

--- src/main/java/net/wurstclient/mixin/GenericContainerScreenMixin.java
+++ src/main/java/net/wurstclient/mixin/GenericContainerScreenMixin.java
@@ -12,6 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.*;
+import net.minecraft.item.ItemStack;
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.ingame.ScreenHandlerProvider;
