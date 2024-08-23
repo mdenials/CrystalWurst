0b43f32c02880c7c67da187be8ac25c9a0f0c617
mdenials <46415607+mdenials@users.noreply.github.com>
2024-08-24 01:46:12 +0700

    Update HitboxHack.java

--- src/main/java/net/wurstclient/hacks/HitboxHack.java
+++ src/main/java/net/wurstclient/hacks/HitboxHack.java
@@ -12,23 +12,21 @@ import net.wurstclient.settings.filterlists.EntityFilterList;
 @SearchTags({"hitbox", "hitboxes", "boxes"})
 public final class HitboxHack extends Hack
 {
-  public final SliderSetting sv = new SliderSetting("XZ Value", 1, 0, 20, 0.000001, ValueDisplay.DECIMAL);
-  public final SliderSetting hv = new SliderSetting("Y Value", 1, 0, 20, 0.000001, ValueDisplay.DECIMAL);
-	
-  private final EntityFilterList entityFilters = EntityFilterList.genericCombat();
+ 	public final SliderSetting sv = new SliderSetting("XZ Value", 1, 0, 20, 0.000001, ValueDisplay.DECIMAL);
+  	public final SliderSetting hv = new SliderSetting("Y Value", 1, 0, 20, 0.000001, ValueDisplay.DECIMAL);
+  	private final EntityFilterList entityFilters = EntityFilterList.genericCombat();
 
 	public HitboxHack()
 	{
 		super("Hitbox");
 		setCategory(Category.MOVEMENT);
 		addSetting(sv);
-        addSetting(hv);
-
-        entityFilters.forEach(this::addSetting);
+        	addSetting(hv);
+		entityFilters.forEach(this::addSetting);
 	}
 
-   public boolean shouldBeExpand(Entity entity)
+   	public boolean shouldBeExpand(Entity entity)
 	{
-	return isEnabled() && entityFilters.testOne(entity);
+		return isEnabled() && entityFilters.testOne(entity);
 	}
 }
