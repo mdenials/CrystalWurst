e97f89aeaff0426ac9d8c96599f9ce62f85dd1fa
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-05 12:21:44 +0500

    Update PathFinder.java

--- src/main/java/net/wurstclient/ai/PathFinder.java
+++ src/main/java/net/wurstclient/ai/PathFinder.java
@@ -463,6 +463,8 @@ public class PathFinder
 				costs[i] += mineCost;
 			if(isMineable(pos.up()))
 				costs[i] += mineCost;
+			if(isMineable(pos.down()))
+				costs[i] += mineCost;
 		}
 		
 		float cost = costs[0] + costs[1];
