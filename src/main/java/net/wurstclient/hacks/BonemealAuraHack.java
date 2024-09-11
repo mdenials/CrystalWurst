56ef81493cbbfac714f302dcbc87da2e2f893d89
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-11 22:46:29 +0500

    Update BonemealAuraHack.java

--- src/main/java/net/wurstclient/hacks/BonemealAuraHack.java
+++ src/main/java/net/wurstclient/hacks/BonemealAuraHack.java
@@ -210,7 +210,7 @@ public final class BonemealAuraHack extends Hack implements PostMotionListener
 		// face and right click the block
 		MC.itemUseCooldown = 4;
 		WURST.getRotationFaker().faceVectorPacket(params.hitVec());
-		InteractionSimulator.rightClickBlock(params.toHitResult());
+		InteractionSimulator.rightClickBlock(params.toHitResult(), Hand.MAIN_HAND);
 		return true;
 	}
 	
@@ -222,8 +222,7 @@ public final class BonemealAuraHack extends Hack implements PostMotionListener
 			return false;
 		
 		// right click the block
-		InteractionSimulator.rightClickBlock(params.toHitResult(),
-			SwingHand.OFF);
+		InteractionSimulator.rightClickBlock(params.toHitResult(), Hand.MAIN_HAND);
 		return true;
 	}
 	
