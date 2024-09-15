917100966d9b66b49064f55d187ca88aa612fe53
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-15 15:19:00 +0500

    Update AutoBuildHack.java

--- src/main/java/net/wurstclient/hacks/AutoBuildHack.java
+++ src/main/java/net/wurstclient/hacks/AutoBuildHack.java
@@ -61,13 +61,7 @@ public final class AutoBuildHack extends Hack
 				+ "Slower but can help with anti-cheat plugins.",
 			Facing.OFF);
 
-	private final SwingHandSetting swingHand = new SwingHandSetting("How AutoBuild should swing your hand when placing blocks.\n\n"
-			+ "\u00a7lOff\u00a7r - Don't swing your hand at all. Will be detected"
-			+ " by anti-cheat plugins.\n\n"
-			+ "\u00a7lServer-side\u00a7r - Swing your hand on the server-side,"
-			+ " without playing the animation on the client-side.\n\n"
-			+ "\u00a7lClient-side\u00a7r - Swing your hand on the client-side."
-			+ " This is the most legit option.");
+	private final SwingHandSetting swingHand = new SwingHandSetting(this, SwingHand.CLIENT);
 	
 	private final CheckboxSetting checkLOS = new CheckboxSetting(
 		"Check line of sight",
