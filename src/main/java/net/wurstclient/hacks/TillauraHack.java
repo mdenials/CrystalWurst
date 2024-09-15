6e8de317c8d650f8020ff1cf4800d710a47fbdda
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-15 15:29:12 +0500

    Update TillauraHack.java

--- src/main/java/net/wurstclient/hacks/TillauraHack.java
+++ src/main/java/net/wurstclient/hacks/TillauraHack.java
@@ -49,13 +49,8 @@ public final class TillauraHack extends Hack implements PostMotionListener
 				+ "Slower but can help with anti-cheat plugins.",
 			Facing.OFF);
 
-	private final SwingHandSetting swingHand = new SwingHandSetting("How AutoBuild should swing your hand when placing blocks.\n\n"
-			+ "\u00a7lOff\u00a7r - Don't swing your hand at all. Will be detected"
-			+ " by anti-cheat plugins.\n\n"
-			+ "\u00a7lServer-side\u00a7r - Swing your hand on the server-side,"
-			+ " without playing the animation on the client-side.\n\n"
-			+ "\u00a7lClient-side\u00a7r - Swing your hand on the client-side."
-			+ " This is the most legit option.");
+	private final SwingHandSetting swingHand =
+		new SwingHandSetting(this, SwingHand.CLIENT);
 	
 	private final CheckboxSetting multiTill =
 		new CheckboxSetting("MultiTill", "Tills multiple blocks at once.\n"
