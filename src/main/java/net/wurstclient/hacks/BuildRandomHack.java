3fa1db71360de94981b9329dea7385681c2794cc
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-15 15:29:56 +0500

    Update BuildRandomHack.java

--- src/main/java/net/wurstclient/hacks/BuildRandomHack.java
+++ src/main/java/net/wurstclient/hacks/BuildRandomHack.java
@@ -27,6 +27,7 @@ import net.wurstclient.settings.FacingSetting;
 import net.wurstclient.settings.SliderSetting;
 import net.wurstclient.settings.SliderSetting.ValueDisplay;
 import net.wurstclient.settings.SwingHandSetting;
+import net.wurstclient.settings.SwingHandSetting.SwingHand;
 import net.wurstclient.util.BlockPlacer;
 import net.wurstclient.util.BlockPlacer.BlockPlacingParams;
 import net.wurstclient.util.BlockUtils;
