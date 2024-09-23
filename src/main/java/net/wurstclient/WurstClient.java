2a0e8e0586816e2619f05bd853a8d10bfa8e08a1
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-23 13:11:20 +0500

    Update WurstClient.java

--- src/main/java/net/wurstclient/WurstClient.java
+++ src/main/java/net/wurstclient/WurstClient.java
@@ -47,9 +47,6 @@ public enum WurstClient
 	public static MinecraftClient MC;
 	public static IMinecraftClient IMC;
 	
-	public static final String VERSION = "7.45";
-	public static final String MC_VERSION = "1.21.1";
-	
 	private EventManager eventManager;
 	private AltManager altManager;
 	private HackList hax;
