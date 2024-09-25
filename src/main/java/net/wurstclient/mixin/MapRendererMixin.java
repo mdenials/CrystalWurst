0cc22d3d54ac34f9ba7ca3dc3dca29cb0ab857a0
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-25 12:40:05 +0500

    Create MapRendererMixin.java

---
+++ src/main/java/net/wurstclient/mixin/MapRendererMixin.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2014-2024 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+
+import net.minecraft.client.render.MapRenderer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.map.MapDecoration;
+
+@Mixin(MapRenderer.MapTexture.class)
+public abstract class MapRendererMixin {
+    @ModifyExpressionValue(method = "draw(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ZI)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/map/MapState;getDecorations()Ljava/lang/Iterable;"))
+    private Iterable<MapDecoration> getIconsProxy(Iterable<MapDecoration> original) {
+      HackList hax = WurstClient.INSTANCE.getHax();
+        return (hax.noMapOverlayHack.isEnabled() && hax.noMapOverlayHack.noMapMarkers.isChecked()) ? EmptyIterator::new : original;
+    }
+
+    @Inject(method = "draw(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ZI)V", at = @At("HEAD"), cancellable = true)
+    private void onDraw(MatrixStack matrices, VertexConsumerProvider vertexConsumers, boolean hidePlayerIcons, int light, CallbackInfo ci) {
+      HackList hax = WurstClient.INSTANCE.getHax();
+        if (hax.noMapOverlayHack.isEnabled() && hax.noMapOverlayHack.noMapContents.isChecked()) 
+          ci.cancel();
+    }
+}
