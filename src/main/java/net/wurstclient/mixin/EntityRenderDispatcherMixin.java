4f62cae23302141901d26cacc09c69049fe7d252
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-25 12:11:30 +0500

    Create EntityRenderDispatcherMixin.java

---
+++ src/main/java/net/wurstclient/mixin/EntityRenderDispatcherMixin.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2014-2024 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.WorldView;
+
+import net.wurstclient.WurstClient;
+
+@Mixin(EntityRenderDispatcher.class)
+public abstract class EntityRenderDispatcherMixin 
+{
+    @Inject(method = "renderShadow", at = @At("HEAD"), cancellable = true)
+    private static void onRenderShadow(MatrixStack matrices, VertexConsumerProvider vertexConsumers,
+      Entity entity, float opacity,float tickDelta,
+      WorldView world, float radius, CallbackInfo info)
+    {
+        if (PostProcessShaders.rendering) 
+          info.cancel();
+        if (WurstClient.INSTANCE.getHax().noDeadEntitiesHack.isEnabled() && entity instanceof LivingEntity && ((LivingEntity) entity).isDead()) 
+          info.cancel();
+    }
+}
+
+
+
+
