74b3a1052884c1a785b85ddddedcd853c211f6f6
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-25 12:27:54 +0500

    Create EnchantingTableBlockEntityRendererMixin.java

---
+++ src/main/java/net/wurstclient/mixin/EnchantingTableBlockEntityRendererMixin.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2014-2024 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.mixin;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.block.entity.EnchantingTableBlockEntityRenderer;
+import net.minecraft.client.render.entity.model.BookModel;
+import net.minecraft.client.util.math.MatrixStack;
+
+import net.wurstclient.WurstClient;
+import net.wurstclient.hack.HackList;
+
+@Mixin(EnchantingTableBlockEntityRenderer.class)
+public abstract class EnchantingTableBlockEntityRendererMixin
+{
+
+  @WrapWithCondition(method = "render(Lnet/minecraft/block/entity/EnchantingTableBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V",
+    at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/model/BookModel;renderBook(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;III)V"))
+    private boolean onRenderBookModelRenderProxy(BookModel instance, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, int i) {
+        HackList hax = WurstClient.INSTANCE.getHax();
+        return !(hax.noEntityWithBlockHack.isEnabled() && hax.noEntityWithBlockHack.noEnchTableBook.isChecked());
+    }  
+}
