7200d9fa5ccf4f86bba440d7105f3ab1a40d5fff
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-25 13:53:34 +0700

    Add files via upload

---
+++ src/main/java/net/wurstclient/mixin/SignBlockEntityRendererMixin.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2014-2024 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+package net.wurstclient.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+
+import net.minecraft.client.render.block.entity.SignBlockEntityRenderer;
+
+import net.wurstclient.WurstClient;
+
+@Mixin(SignBlockEntityRenderer.class)
+public abstract class SignBlockEntityRendererMixin {
+    @ModifyExpressionValue(method = "renderText", at = @At(value = "CONSTANT", args = {"intValue=4", "ordinal=1"}))
+    private int loopTextLengthProxy(int i) {
+
+        if () 
+            return 0;
+        return i;
+    }
+}
