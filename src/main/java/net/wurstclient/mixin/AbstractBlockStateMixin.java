dc0f7b41fc726f9e5818ff5451db538396824727
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-25 20:09:55 +0500

    Update AbstractBlockStateMixin.java

--- src/main/java/net/wurstclient/mixin/AbstractBlockStateMixin.java
+++ src/main/java/net/wurstclient/mixin/AbstractBlockStateMixin.java
@@ -118,7 +118,7 @@ public abstract class AbstractBlockStateMixin extends State<Block, BlockState>
     	private BlockPos modifyPos(BlockPos pos)
 	{
 		HackList hax = WurstClient.INSTANCE.getHax();
-        	if (hax.noTextureRotationsHack.isEnabled())
+        	if (hax != null && hax.noTextureRotationsHack.isEnabled())
 			return pos.multiply(RANDOM.nextInt());
 		
         	return pos;
