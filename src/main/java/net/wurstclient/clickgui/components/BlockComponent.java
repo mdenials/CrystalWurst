00f4acac9be0ed0129a10cdff369c97fd22cbba2
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-26 16:42:40 +0500

    Update BlockComponent.java

--- src/main/java/net/wurstclient/clickgui/components/BlockComponent.java
+++ src/main/java/net/wurstclient/clickgui/components/BlockComponent.java
@@ -66,7 +66,7 @@ public final class BlockComponent extends Component
 		float partialTicks)
 	{
 		ClickGui gui = WurstClient.INSTANCE.getGui();
-		float[] bgColor = gui.getBgColor();
+		int[] bgColor = gui.getBgColor();
 		int txtColor = gui.getTxtColor();
 		float opacity = gui.getOpacity();
 		
