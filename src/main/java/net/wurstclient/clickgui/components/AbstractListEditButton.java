a689bd87b01a5ce9a20c557cf11c271e671b3b07
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-26 16:13:01 +0500

    Update AbstractListEditButton.java

--- src/main/java/net/wurstclient/clickgui/components/AbstractListEditButton.java
+++ src/main/java/net/wurstclient/clickgui/components/AbstractListEditButton.java
@@ -92,7 +92,7 @@ public abstract class AbstractListEditButton extends Component
 			gui.setTooltip(getSetting().getWrappedDescription(200));
 		
 		// background
-		RenderUtils.setShaderColor(bgColor, opacity);
+		RenderUtils.setShaderColor(bgColor);
 		BufferBuilder bufferBuilder = tessellator
 			.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION);
 		bufferBuilder.vertex(matrix, x1, y1, 0);
@@ -102,7 +102,7 @@ public abstract class AbstractListEditButton extends Component
 		BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
 		
 		// box
-		RenderUtils.setShaderColor(bgColor, hBox ? opacity * 1.5F : opacity);
+		RenderUtils.setShaderColor(bgColor);
 		bufferBuilder = tessellator.begin(VertexFormat.DrawMode.QUADS,
 			VertexFormats.POSITION);
 		bufferBuilder.vertex(matrix, x3, y1, 0);
@@ -110,7 +110,7 @@ public abstract class AbstractListEditButton extends Component
 		bufferBuilder.vertex(matrix, x2, y2, 0);
 		bufferBuilder.vertex(matrix, x2, y1, 0);
 		BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
-		RenderUtils.setShaderColor(acColor, 0.5F);
+		RenderUtils.setShaderColor(acColor);
 		bufferBuilder = tessellator.begin(
 			VertexFormat.DrawMode.DEBUG_LINE_STRIP, VertexFormats.POSITION);
 		bufferBuilder.vertex(matrix, x3, y1, 0);
