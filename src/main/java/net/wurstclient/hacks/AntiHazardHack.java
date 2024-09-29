235f81da1a6e38e503bc7c81d874ec1cb9fbf067
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-29 20:34:10 +0500

    Update AntiHazardHack.java

--- src/main/java/net/wurstclient/hacks/AntiHazardHack.java
+++ src/main/java/net/wurstclient/hacks/AntiHazardHack.java
@@ -49,7 +49,8 @@ public final class AntiHazardHack extends Hack implements BlockCollisionShapeLis
 	@Override
 	public void onBlockCollisionShape(BlockPos pos, Block block, BlockCollisionShapeEvent event)
 	{
-        	if(hazards.contains(event.getCollisionBlock()))
+		BlockPos pos = event.getCollisionBlockPos()
+        	if(hazards.contains(BlockUtils.getName(pos)))
 			event.setCollisionShape(VoxelShapes.fullCube());
 	}
 }
