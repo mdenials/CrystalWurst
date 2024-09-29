e27a267b2ac57d7aa4c6c30cd35373b9175e6aa7
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-29 17:26:41 +0500

    Create BlockCollisionShapeListener.java

---
+++ src/main/java/net/wurstclient/events/BlockCollisionShapeListener.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2014-2024 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.events;
+
+import java.util.ArrayList;
+
+import net.minecraft.util.shape.VoxelShape;
+import net.wurstclient.event.Event;
+import net.wurstclient.event.Listener;
+
+public interface BlockCollisionShapeListener extends Listener
+{
+	public void onBlockCollisionShape(BlockCollisionShapeEvent event);
+	
+	public static class BlockCollisionShapeEvent
+		extends Event<BlockCollisionShapeListener>
+	{
+		private VoxelShape collisionShape;
+		
+		public VoxelShape getCollisionShape()
+		{
+			return collisionShape;
+		}
+		
+		public void setCollisionShape(VoxelShape collisionShape)
+		{
+			this.collisionShape = collisionShape;
+		}
+		
+		@Override
+		public void fire(ArrayList<BlockCollisionShapeListener> listeners)
+		{
+			for(BlockCollisionShapeListener listener : listeners)
+				listener.onCactusCollisionShape(this);
+		}
+		
+		@Override
+		public Class<BlockCollisionShapeListener> getListenerType()
+		{
+			return BlockCollisionShapeListener.class;
+		}
+	}
+}
