eb433e46aa43c0256e8d11bf2a4d7492600bffd4
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-29 17:59:05 +0500

    Update BlockCollisionShapeListener.java

--- src/main/java/net/wurstclient/events/BlockCollisionShapeListener.java
+++ src/main/java/net/wurstclient/events/BlockCollisionShapeListener.java
@@ -9,29 +9,54 @@ package net.wurstclient.events;
 
 import java.util.ArrayList;
 
+import net.minecraft.block.Block;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.wurstclient.event.Event;
 import net.wurstclient.event.Listener;
 
 public interface BlockCollisionShapeListener extends Listener
 {
-	public void onBlockCollisionShape(BlockCollisionShapeEvent event);
+	public void onBlockCollisionShape(BlockPos pos, Block block, BlockCollisionShapeEvent event);
 	
 	public static class BlockCollisionShapeEvent
 		extends Event<BlockCollisionShapeListener>
 	{
+		
+		private BlockPos blockPos;
+		private Block block;
 		private VoxelShape collisionShape;
+		
+		public VoxelShape getCollisionBlockPos()
+		{
+			return blockPos;
+		}
+
+		public VoxelShape getCollisionBlock()
+		{
+			return block;
+		}
 		
 		public VoxelShape getCollisionShape()
 		{
 			return collisionShape;
+		}
+
+		public void setBlockPos(BlockPos pos)
+		{
+			this.blockPos = blockPos;
+		}
+
+		public void setBlock(Block block)
+		{
+			this.block = block;
 		}
 		
 		public void setCollisionShape(VoxelShape collisionShape)
 		{
 			this.collisionShape = collisionShape;
 		}
-		
+
 		@Override
 		public void fire(ArrayList<BlockCollisionShapeListener> listeners)
 		{
