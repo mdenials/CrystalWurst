83d385a20beb7a579a320fdd5be06d208abb4671
Dart Vladimir <46415607+mdenials@users.noreply.github.com>
2024-09-29 20:40:37 +0700

    Add files via upload

---
+++ src/main/java/net/wurstclient/hacks/AntiHazardHack.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2014-2024 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.hacks;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import net.minecraft.block.Block;
+
+import net.minecraft.util.shape.VoxelShapes;
+import net.wurstclient.Category;
+import net.wurstclient.SearchTags;
+import net.wurstclient.events.BlockCollisionShapeListener;
+import net.wurstclient.settings.BlockListSetting;
+import net.wurstclient.hack.Hack;
+import net.wurstclient.util.BlockUtils;
+
+@SearchTags({"NoCactus", "anti cactus", "no cactus"})
+public final class AntiHazardHack extends Hack implements BlockCollisionShapeListener
+{
+    public final BlockListSetting hazards = new BlockListSetting("Hazard List", "The types of hazards blocks to isolate");
+
+	public AntiHazardHack()
+	{
+		super("AntiHazard);
+		setCategory(Category.BLOCKS);
+	}
+	
+	@Override
+	protected void onEnable()
+	{
+		EVENTS.add(BlockCollisionShapeListener.class, this);
+	}
+	
+	@Override
+	protected void onDisable()
+	{
+		EVENTS.remove(BlockCollisionShapeListener.class, this);
+	}
+	
+	@Override
+	public void onBlockCollisionShape(BlockPos pos, Block block, BlockCollisionShapeEvent event)
+	{
+        if(hazards.indexOf(event.getBlock()).contains(BlockUtils.getName(pos)))
+		event.setCollisionShape(VoxelShapes.fullCube());
+	}
+}
